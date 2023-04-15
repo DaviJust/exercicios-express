8aa583486c832c6f87198b64dd54f4d12becc389
Davi <davijustinocouto@gmail.com>
2023-04-15 17:56:33 -0300

    Continuação da aula para requisição e tal

--- index.js
+++ index.js
@@ -6,26 +6,43 @@ const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
-// Endpoint for making a GET request to an external API
-app.get('/api/data', async (req, res) => {
-  try {
-    // Make a GET request to an external API
-    const response = await axios.get('https://dog.ceo/api/breeds/list/all', { responseType: 'arraybuffer' });
-
-    // Extract the data from the response
-    const data = response.data;
-
-    // Set the appropriate content type for the image
-    res.setHeader('Content-Type', 'image/jpeg');
-
-    // Send the image data as the response
-    res.status(200).send(data);
-  } catch (error) {
-    // Handle any errors that may occur
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch data from the external API' });
-  }
+app.get('/opa', (req, res) => {
+  res.json({
+    data: [
+      { id: 7, name: 'Ana', position: 1 },
+      { id: 8, name: 'Bia', position: 2 },
+      { id: 9, name: 'Carlos', position: 3 }
+    ],
+    count: 30,
+    skip: 0,
+    limit: 3,
+    status: 200
+  });
 });
+// Endpoint for making a GET request to an external API
+// app.get('/api/data', async (req, res) => {
+//   try {
+//     // Make a GET request to an external API
+//     const resWithMessageImageUrl = await axios.get('https://dog.ceo/api/breeds/image/random');
+//     console.log("🚀 ~ file: index.js:14 ~ app.get ~ response:", resWithMessageImageUrl)
+
+//     const imageUrl = resWithMessageImageUrl.data.message
+
+//     // Extract the data from the response
+//     // const data = response.data;
+
+//     // Set the appropriate content type for the image
+//     res.send(`<img src="${imageUrl}">`);
+
+//     // Send the image data as the response
+    
+//     // res.sendFile
+//   } catch (error) {
+//     // Handle any errors that may occur
+//     console.error(error);
+//     res.status(500).json({ error: 'Failed to fetch data from the external API' });
+//   }
+// });
 
 // Start the server
 app.listen(PORT, () => {
