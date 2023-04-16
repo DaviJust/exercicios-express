82d836de01db16185460eda1880d019435c99adb
Davi <davijustinocouto@gmail.com>
2023-04-16 18:55:06 -0300

    Término do módulo: Organização para começar api API

--- index.js
+++ index.js
@@ -1,89 +1,76 @@
-// Importing the necessary modules
-const express = require('express');
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 
+const saudacao = require('./saudacaoMid')
+const usuarioApi = require('./api/usuario')
+const produtoApi = require('./api/produto')
+produtoApi(app, 'com param!')
+
+
+app.post('/usuario', usuarioApi.salvar)
+app.get('/usuario', usuarioApi.obter)
+
 app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// Initializing the express app
-const app = express();
-const PORT = 3000;
+app.use(saudacao('Guilherme'))
 
+app.use((req, res, next) => {
+    console.log('Antes...')
+    next()
+})
 
-app.use('/opa', (req, res, next) => {
-  console.log('Antes')
-  next()
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Cliente relatório: completo = ${req.query.completo} ano = ${req.query.ano}`)
 })
 
 app.post('/corpo', (req, res) => {
-  
-  app.use(bodyParser.text())
-  res.send(req.body)
+    // let corpo = ''
+    // req.on('data', function(parte) {
+    //     corpo += parte
+    // })
 
+    // req.on('end', function() {
+    //     res.send(corpo)
+    // })
+    res.send(req.body)
 })
 
-
-app.get('/clientes/relatorio', (req, res) => {
-  res.send(`Cliente relatório: completo ${req.query.completo} ano = ${req.query.ano}`)
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado!`)
 })
 
-app.get('/cliente/:id', (req, res) => {
-  res.send(`Cliente ${req.params.id} selecionado!`)
-})
+app.get('/opa', (req, res, next) => {
+    console.log('Durante...')
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1 },
+            {id: 34, name: 'Bia', position: 2 },
+            {id: 73, name: 'Carlos', position: 3 }
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
 
-
-
-app.use('/opa', (req, res, next) => {
-  console.log('Durante')
-  next()
-})
-
-
-app.use('/opa', (req, res, next) => {
-  console.log('Depois')
-  next()
-})
-
-app.get('/opa', (req, res) => {
-  res.json({
-    data: [
-      { id: 7, name: 'Ana', position: 1 },
-      { id: 8, name: 'Bia', position: 2 },
-      { id: 9, name: 'Carlos', position: 3 }
-    ],
-    count: 30,
-    skip: 0,
-    limit: 3,
-    status: 200
-  });
-});
-
-
-// Endpoint for making a GET request to an external API
-// app.get('/api/data', async (req, res) => {
-//   try {
-//     // Make a GET request to an external API
-//     const resWithMessageImageUrl = await axios.get('https://dog.ceo/api/breeds/image/random');
-//     console.log("🚀 ~ file: index.js:14 ~ app.get ~ response:", resWithMessageImageUrl)
-
-//     const imageUrl = resWithMessageImageUrl.data.message
-
-//     // Extract the data from the response
-//     // const data = response.data;
-
-//     // Set the appropriate content type for the image
-//     res.send(`<img src="${imageUrl}">`);
-
-//     // Send the image data as the response
+    next()
     
-//     // res.sendFile
-//   } catch (error) {
-//     // Handle any errors that may occur
-//     console.error(error);
-//     res.status(500).json({ error: 'Failed to fetch data from the external API' });
-//   }
-// });
+    // res.json({
+    //     name: 'iPad 32Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
+})
+
+app.use((req, res) => {
+    console.log('Depois...')
+})
+
+app.listen(3000, () => {
+    console.log('Backend executando...')
+})
